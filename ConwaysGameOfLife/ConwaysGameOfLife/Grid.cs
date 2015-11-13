1e8948c2a5033b830de083c77007b0c544be0325
wfhutch <wfh2727@gmail.com>
2015-11-13 12:08:08 -0600

    Partner solution intial commit

---
+++ ConwaysGameOfLife/ConwaysGameOfLife/Grid.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConwaysGameOfLife
+{
+    public class Grid
+    {
+        private bool[,] currentBoard;
+        private bool[,] nextBoard;
+        public Grid(bool[,] newBoard)
+        {
+            currentBoard = newBoard;
+            nextBoard = new bool[currentBoard.GetLength(0), currentBoard.GetLength(1)];
+        }
+
+        public Grid() // this was created for CanCreateInstanceOfGrid test
+        {
+        }
+
+        public void Tick()
+        {
+            for (int i = 0; i < currentBoard.GetLength(0); i++)  //looks at each row. use GetLength for 2d arrays.
+            {
+                for (int j = 0; j < currentBoard.GetLength(1); j++) //looks at each column 
+                {
+                    int alive = 0;
+
+                    try { if (currentBoard[i - 1, j] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i - 1, j + 1] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i - 1, j - 1] == true) { alive += 1; } } catch (IndexOutOfRangeException){ };
+                    try { if (currentBoard[i, j + 1] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i + 1, j + 1] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i + 1, j] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i + 1, j - 1] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                    try { if (currentBoard[i, j - 1] == true) { alive += 1; } } catch (IndexOutOfRangeException) { };
+                        
+                    if (currentBoard[i, j] == true)
+                        {
+                            if (alive < 2)
+                            {
+                                nextBoard[i, j] = false;
+                            } else if (alive < 4)
+                            {
+                                nextBoard[i, j] = true;
+                            } else
+                            {
+                                nextBoard[i, j] = false;
+                            }
+                        }
+                        else
+                        {
+                            if (alive == 3)
+                            {
+                                nextBoard[i, j] = true;
+                            } else
+                            {
+                                nextBoard[i, j] = false;
+                            }
+                        }
+                    }
+            }
+
+            currentBoard = nextBoard;
+        }
+        public bool[,] getBoard()
+        {
+            return currentBoard;
+        }
+    }
+}
---
+++ ConwaysGameOfLife/ConwaysTests/ConwaysGOLTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConwaysGameOfLife;
+using System.Collections.Generic;
+
+namespace ConwaysTests
+{
+    [TestClass]
+    public class ConwaysGOLTests
+    {
+
+        [TestMethod]
+        public void CanCreateInstanceOfGrid()
+        {
+            Grid MyGrid = new Grid();
+            Assert.IsNotNull(MyGrid);
+        }
+
+        [TestMethod]
+        public void StillLifeTestSquare()
+        {
+            bool[,] stillLife = new bool[,] { { true, true }, { true, true } };
+            Grid board = new Grid(stillLife);
+            board.Tick();
+            CollectionAssert.AreEqual(stillLife, board.getBoard());
+        }
+
+        [TestMethod]
+        public void StillLifeTestBeehive()
+        {
+            bool[,] beehive = new bool[,] { { false, true, true, false }, { true, false, false, true }, { false, true, true, false } };
+            Grid board = new Grid(beehive);
+            board.Tick();
+            CollectionAssert.AreEqual(beehive, board.getBoard());
+        }
+
+        [TestMethod]
+        public void OscillatorTest1()
+        {
+            bool[,] oscillator = new bool[,] { { false, true, false }, { false, true, false }, { false, true, false } };
+            Grid board = new Grid(oscillator);
+            board.Tick();
+            bool[,] expected = new bool[,] { { false, false, false }, { true, true, true }, { false, false, false } };
+            CollectionAssert.AreEqual(expected, board.getBoard());
+        }
+
+        [TestMethod]
+        public void DiagonalTest1()
+        {
+            bool[,] diagonal = new bool[,] { { true, false, false }, { false, true, false }, { false, false, true } };
+            Grid board = new Grid(diagonal);
+            board.Tick();
+            bool[,] expected = new bool[,] { { false, false, false }, { false, true, false }, { false, false, false } };
+            CollectionAssert.AreEqual(expected, board.getBoard());
+        }
+
+        [TestMethod]
+        public void BeaconTest1()
+        {
+            bool[,] beacon = new bool[,] { { true, true, false, false }, { true, true, false, false }, { false, false, true, true }, { false, false, true, true } };
+            Grid board = new Grid(beacon);
+            board.Tick();
+            bool[,] expected = new bool[,] { { true, true, false, false }, { true, false, false, false }, { false, false, false, true }, { false, false, true, true } };
+            CollectionAssert.AreEqual(expected, board.getBoard());
+        }
+
+        [TestMethod]
+        public void ToadTest1()
+        {
+            bool[,] toad = new bool[,] { { false, false, false, false }, {false, true, true, true}, { true, true, true, false }, { false, false, false, false } };
+            Grid board = new Grid(toad);
+            board.Tick();
+            bool[,] expected = new bool[,] { { false, false, true, false }, { true, false, false, true }, { true, false, false, true }, { false, true, false, false } };
+            CollectionAssert.AreEqual(expected, board.getBoard());
+        }
+    }
+}
+
